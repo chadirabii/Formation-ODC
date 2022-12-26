113ad58c524edb24e59a5f060ab3a46605aca0a0
chadirabii <chadirabii@gmail.com>
2022-12-26 20:35:31 +0100

    fix the logo

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -4,13 +4,17 @@ import orange from "../assets/orange.png";
 export default function Navbar() {
   return (
     <div className="shadow-xl navbar bg-lightBlue text-light ">
-      <div className="flex-1 ">
-        <a
-          href="Home"
-          className="flex flex-row items-center text-2xl font-bold gap-x-3"
-        >
+      <div className="flex-1">
+        <a href="#" className="flex flex-row items-center text-2xl gap-x-3">
           <img src={orange} width="50" alt="" />
-          Orange Data Center
+        </a>
+        <a href="" className="flex">
+          <span className="flex flex-row items-center ml-1 text-2xl font-bold text-orange-400">
+            Orange
+          </span>
+          <span className="flex flex-row items-center ml-1 text-2xl font-bold">
+            Digital Center
+          </span>
         </a>
       </div>
       <div className="flex-none">
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-row flex-wrap justify-between gap-5 ">
       <UserCard />
-  
+      
     </div>
   );
 }
